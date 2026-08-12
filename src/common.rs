1801110647ac79390f1e572316c34fc7234cf8a6
pavel9619229 <223954535+pavel9619229-cmyk@users.noreply.github.com>
2026-08-13 01:02:44 +0300

    fix: correct Masha server public key

--- src/common.rs
+++ src/common.rs
@@ -57,7 +57,7 @@ pub const PORTABLE_APPNAME_RUNTIME_ENV_KEY: &str = "RUSTDESK_APPNAME";
 pub const TECHNICAL_APP_NAME: &str = "masha-remote-operator";
 pub const DISPLAY_APP_NAME: &str = "УДАЛЕННЫЙ ОПЕРАТОР МАША";
 pub const MASHA_RENDEZVOUS_SERVER: &str = "77.222.38.70";
-pub const MASHA_SERVER_PUBLIC_KEY: &str = "9ceMofgvYTVTIC95mhjgmTejoqprMLiaMONVQJo8I=";
+pub const MASHA_SERVER_PUBLIC_KEY: &str = "9ceMofgvYTVTIC95mhjgmTejoqprML2iaMONVQJo8I=";
 
 pub fn configure_masha_servers() {
     *config::PROD_RENDEZVOUS_SERVER.write().unwrap() = MASHA_RENDEZVOUS_SERVER.to_owned();
